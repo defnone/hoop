9131b58aa91dc54e2c6cf112d4c3d44c4949bbc5
defnone <141522332+defnone@users.noreply.github.com>
2025-10-10 18:07:14 +0700

    client: refactor and enhance search functionality with new filters

--- client/src/components/search/SearchTable/SearchSearchBar.tsx
+++ client/src/components/search/SearchTable/SearchSearchBar.tsx
@@ -1,37 +1,102 @@
-import { Input } from '@/components/ui/input';
-import { ListFilter } from 'lucide-react';
-import { Dispatch, SetStateAction } from 'react';
+import { Button } from '@/components/ui/button';
+import { ButtonGroup } from '@/components/ui/button-group';
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from '@/components/ui/input-group';
+import { ArrowLeft, Eraser, ListFilter } from 'lucide-react';
+import { Dispatch, SetStateAction, useEffect } from 'react';
 
 interface SearchSearchBarProps {
   search: string;
   setSearch: Dispatch<SetStateAction<string>>;
   isLoading: boolean;
+  currentSeason: number | string;
 }
 
 export function SearchTopBar({
   search,
   setSearch,
   isLoading,
+  currentSeason,
 }: SearchSearchBarProps) {
+  useEffect(() => {
+    if (search.includes('S'))
+      setSearch((prev) => prev.replace(/['S'][\d]/i, `S${currentSeason}`));
+  }, [currentSeason]);
   return (
-    <div className='flex items-center pt-5 w-full'>
+    <div className='flex items-center pt-7 w-full'>
       <div className='flex items-center gap-2 w-full'>
-        <div className='relative'>
-          <Input
-            disabled={isLoading}
-            placeholder='Filter by title (or part of it)'
-            className='w-64 rounded-md pl-8'
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <ListFilter
-            size={16}
-            className='absolute left-2 top-1/2 -translate-y-1/2 text-muted-foreground'
-          />
-        </div>
+        <ButtonGroup className='relative flex'>
+          <InputGroup className='w-72 rounded-md h-10'>
+            <InputGroupAddon align={'inline-start'}>
+              <ListFilter size={17} className='' />
+            </InputGroupAddon>
+            <InputGroupInput
+              disabled={isLoading}
+              placeholder='Filter by title (or part of it)'
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </InputGroup>
+          <Button
+            size={'icon-lg'}
+            variant={'outline'}
+            onClick={() => setSearch('')}>
+            <Eraser strokeWidth={3} className='w-4 h-4 text-zinc-400' />
+          </Button>
+        </ButtonGroup>
+
+        <ArrowLeft strokeWidth={3} className='w-4 h-4 ml-2 text-zinc-400' />
+
+        <ButtonGroup className='h-10 flex items-center justify-center ml-2'>
+          {currentSeason && (
+            <Button
+              variant={'outline'}
+              size={'lg'}
+              onClick={() => setSearch((prev) => `S${currentSeason} ${prev}`)}
+              disabled={search.includes('S')}>
+              S{currentSeason}
+            </Button>
+          )}
+          <Button
+            variant={'outline'}
+            size={'lg'}
+            onClick={() =>
+              setSearch((prev) =>
+                prev.includes('S') ? `S${currentSeason} 720p` : `720p`
+              )
+            }
+            disabled={search.includes('720p')}>
+            720p
+          </Button>
+          <Button
+            variant={'outline'}
+            size={'lg'}
+            onClick={() =>
+              setSearch((prev) =>
+                prev.includes('S') ? `S${currentSeason} 1080p` : `1080p`
+              )
+            }
+            disabled={search.includes('1080p')}>
+            1080p
+          </Button>
+          <Button
+            variant={'outline'}
+            size={'lg'}
+            onClick={() =>
+              setSearch((prev) =>
+                prev.includes('S') ? `S${currentSeason} 2160p` : `2160p`
+              )
+            }
+            disabled={search.includes('2160p')}>
+            2160p
+          </Button>
+        </ButtonGroup>
       </div>
 
-      <div className='flex items-center gap-2'></div>
+      <div className='flex items-center gap-2 mr-auto'></div>
     </div>
   );
 }
