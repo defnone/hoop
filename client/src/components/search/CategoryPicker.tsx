13600bb69f30c62df53564892136e6176fbbf891
defnone <141522332+defnone@users.noreply.github.com>
2025-10-10 19:51:08 +0700

    client: update styles and enhance CategoryPicker with popover for category information

--- client/src/components/search/CategoryPicker.tsx
+++ client/src/components/search/CategoryPicker.tsx
@@ -2,6 +2,8 @@ import { useId } from 'react';
 import { ButtonGroup } from '../ui/button-group';
 import { Button } from '../ui/button';
 import { cn } from '@/lib/utils';
+import { BadgeQuestionMark } from 'lucide-react';
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 
 export default function CategoryPicker({
   category,
@@ -20,19 +22,39 @@ export default function CategoryPicker({
   ];
 
   return (
-    <ButtonGroup>
-      {items.map((item) => (
-        <Button
-          className={cn(
-            'font-bold',
-            category === item.value && 'border border-border'
-          )}
-          variant={category === item.value ? 'secondary' : 'outline'}
-          key={`${id}-${item.value}`}
-          onClick={() => setCategory(item.value)}>
-          {item.label}
-        </Button>
-      ))}
+    <ButtonGroup className='items-center'>
+      <ButtonGroup>
+        {items.map((item) => (
+          <Button
+            className={cn(
+              'font-bold',
+              category === item.value && 'border border-border'
+            )}
+            variant={category === item.value ? 'secondary' : 'outline'}
+            key={`${id}-${item.value}`}
+            onClick={() => setCategory(item.value)}>
+            {item.label}
+          </Button>
+        ))}
+      </ButtonGroup>
+      <ButtonGroup>
+        <Popover>
+          <PopoverTrigger>
+            <Button size={'icon-sm'} variant={'ghost'} className='rounded-full'>
+              <BadgeQuestionMark />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent
+            sideOffset={10}
+            align='start'
+            className='text-sm shadow-lg'>
+            These are the categories sent to Jackett for searching on trackers.
+            They don’t always work effectively for category-based searches. It’s
+            usually best to use “All” and then filter the search results using
+            the filters below.
+          </PopoverContent>
+        </Popover>
+      </ButtonGroup>
     </ButtonGroup>
   );
 }
